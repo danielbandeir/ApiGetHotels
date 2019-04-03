aad0d3be9ab09d034f275155e2b11651e9bda847
Daniel Bandeira <bandeirapbdaniel@gmail.com>
2019-04-03 08:58:11 -0300

    Static added

--- restHotels/settings.py
+++ restHotels/settings.py
@@ -121,6 +121,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
+STATIC_ROOT = "/staticfiles/"
+
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10
